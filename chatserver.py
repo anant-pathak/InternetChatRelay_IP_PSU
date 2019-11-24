f32a88ae931176b49382a65dc5ac8bf1df222ab8
Melville1851 <kinetelos@gmail.com>
2019-11-23 22:43:15 -0800

    fixed problem with loop where it would execute int() on an empty string

--- chatserver.py
+++ chatserver.py
@@ -16,16 +16,18 @@ length_header = 100
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host,port))
 server.listen(backlog)
-input = [server]
+input = [server,sys.stdin]
 running = 1
 
 def get_message(client):
     #"client" must be a socket object connected to a client
     length = client.recv(length_header)
-    if not length:
-        s.close()
-        input.remove(s)                                                
-    msg = client.recv(int(length.decode().strip()))
+    length = length.decode()
+    
+    if length == '':
+        print(length)
+        return False                                                
+    msg = client.recv(int(length.strip()))
     return msg.decode()
 
 while running:
@@ -37,27 +39,22 @@ while running:
             # handle the server socket
             client, address = server.accept()
             input.append(client)
-
         elif s == sys.stdin:
             # handle standard input
             junk = sys.stdin.readline()
             running = 0 
         else:
             # handle all other sockets
-            #arbitrary = 10               Anant - Uncomment these and you will see that this section is getting executed twice
-            #print(arbitrary)                     I have to figure out why my gdb is messed up so i dont have to "print debug" haha
-            #arbitrary = arbitrary + 1
-            #print(arbitrary)
             inputMsg = get_message(s)
-            # Here I think we would  pass it to the  message handler
-            # The message handler will process the data appropriately and then queue it for output.
-            # I'm thinking that Rooms object will keep a list of client in that room and when a message is sent
-            # to that room the handler will then broadcast the message to each of the client sockets.
-            outputMsg = inputMsg.encode()
-            if outputMsg:
-                s.send(outputMsg)  #look up exactly functionality
-            else:
+            if not inputMsg:
                 s.close()
                 input.remove(s)
+            else:
+                outputMsg = inputMsg.encode()
+                # Here I think we would  pass it to the  message handler
+                # The message handler will process the data appropriately and then queue it for output.
+                # I'm thinking that Rooms object will keep a list of client in that room and when a message is sent
+                # to that room the handler will then broadcast the message to each of the client sockets.
+                s.send(outputMsg)  #look up exactly functionality      
+            
 server.close()
-
