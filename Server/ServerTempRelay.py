9146a9ffd8b23711ac02e36ec9a6fd687cfa6548
introsec-pathak <pathak@pdx.edu>
2019-12-01 00:17:11 -0800

    third small update

--- Server/ServerTempRelay.py
+++ Server/ServerTempRelay.py
@@ -1,9 +1,44 @@
 import socket
 from datetime import datetime
 import select
+import sys
+import json
 
 sock_add = ('localhost', 6789)
 max_size = 4096
+
+#Chat rooms
+chatroom_usergroup = {"one": [],  #"one@gmail.com", "two@gmail.com"
+                      }
+chatroom_messages = {
+    "one": [],
+}
+# chatroom_update_available = {
+#     "one" : False,
+# }
+
+def create_room():
+    pass
+def list_room():
+    pass
+def join_room():
+    pass
+def leave_room():
+    pass
+def list_members():
+    pass
+def sendMsg_room(username, msg, room):
+    if room in chatroom_messages: #it will check for the "room" among keys of chatroom_messages
+        #also check for a valid username.
+        chatroom_messages[room].append((datetime.now(),username,msg))
+        # chatroom_update_available[room] = True
+def getMsg_room(room, username):
+    if room in chatroom_messages:
+        # if chatroom_update_available[room] == True:
+        return chatroom_messages[room]
+
+def disconnectfrom_room(): #<----- Implement at client itself.
+
 
 #UDP
 # print("starting the server at ", datetime.now())
@@ -25,6 +60,7 @@ inputSockets = []
 outputSockets = []
 
 inputSockets.append(serverSocket)
+inputSockets.append(sys.stdin)
 
 def myreceive(clientSocket):
     chunks = []
@@ -54,13 +90,19 @@ while 1:
             (clientSocket, clientAddress) = serverSocket.accept()
             clientSocket.setblocking(0)
             inputSockets.append(clientSocket)
+            #FOR DEMO PURPOSE BY DEFAULT EVERY SOCKET JOINS THE CHAT ROOM : "ONE"
+            chatroom_usergroup["one"].append(clientSocket)
+        elif s == sys.stdin:
+            junk = sys.stdin.readline()
+            print("Entered text was:", junk)
         else: # the socket in readable is a client socket having sent some data.
             clientMsg = myreceive(s)
             if clientMsg:
                 print("client: ", clientSocket, "said: ", clientMsg)
-                s.sendall(b"your message received")
+                s.sendall(b"Msg received")
             else:
                 inputSockets.remove(s)
+                chatroom_usergroup["one"].remove(s)
                 s.close()
             # inputSockets.remove(s)
             # s.close()
